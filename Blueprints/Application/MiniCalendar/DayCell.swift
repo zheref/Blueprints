630880690bcc68c9aec12a93e3a0b15de8b2f87d
Sergio Daniel L'Garcia <zheref@gmail.com>
2022-08-25 13:08:53 -0500

    Fix bug with selection

--- Blueprints/Application/MiniCalendar/DayCell.swift
+++ Blueprints/Application/MiniCalendar/DayCell.swift
@@ -35,21 +35,8 @@ class DayCell: UICollectionViewCell {
     }
     
     func setSelected(_ selected: Bool) {
-        if selected {
-            if let model = model, model.day is RedDay {
-                contentView.backgroundColor = UIColor.clear
-                contentView.outline(withWidth: 3.0, andColor: K.Color.rogueSelection)
-            } else {
-                contentView.backgroundColor = K.Color.mainSelection
-                contentView.outline(withWidth: 0)
-            }
-        } else if let model = model, model.day.date.isToday {
-            contentView.backgroundColor = K.Color.rogueSelection
-            contentView.outline(withWidth: 0)
-        } else {
-            contentView.backgroundColor = UIColor.clear
-            contentView.outline(withWidth: 0)
-        }
+        guard let model = model else { return }
+        resolveDecoration(while: selected, today: model.day.date.isToday)
     }
     
     private func bind() {
@@ -61,12 +48,30 @@ class DayCell: UICollectionViewCell {
         blueprintDayLabel?.text = model.blueprintDayTitle
         contentView.round(withRadius: 12)
         
-        if model.day.date.isToday {
+        resolveDecoration(while: isSelected, today: model.day.date.isToday)
+    }
+    
+    private func resolveDecoration(while selected: Bool, today: Bool) {
+        if selected {
+            actualDayLabel.font = UIFont(name: K.Font.avenirMedium, size: 12.0)
+            blueprintDayLabel.font = UIFont(name: K.Font.avenirMedium, size: 12.0)
+            if let model = model, model.day is RedDay {
+                contentView.backgroundColor = UIColor.clear
+                contentView.outline(withWidth: 3.0, andColor: K.Color.rogueSelection)
+            } else {
+                contentView.backgroundColor = K.Color.mainSelection
+                contentView.outline(withWidth: 0)
+            }
+        } else if let model = model, model.day.date.isToday {
             actualDayLabel.font = UIFont(name: K.Font.avenirMedium, size: 12.0)
             blueprintDayLabel.font = UIFont(name: K.Font.avenirMedium, size: 12.0)
             contentView.backgroundColor = K.Color.rogueSelection
+            contentView.outline(withWidth: 0)
         } else {
+            actualDayLabel.font = UIFont(name: K.Font.avenirBook, size: 11.0)
             blueprintDayLabel.font = UIFont(name: K.Font.avenirBook, size: 11.0)
+            contentView.backgroundColor = UIColor.clear
+            contentView.outline(withWidth: 0)
         }
     }
     
